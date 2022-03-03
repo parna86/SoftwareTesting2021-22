fcc0d26f751caa89e958a4f96bcf67807a34b50c
Ap0806 <57600509+parna86@users.noreply.github.com>
2022-03-03 15:45:10 +0000

    two bugs found

--- src/test/java/st/Task1_1_FunctionalTest.java
+++ src/test/java/st/Task1_1_FunctionalTest.java
@@ -14,10 +14,23 @@ public class Task1_1_FunctionalTest {
 		parser = new Parser();
 	}
 	
-	@Test
-	public void testOne() {
-		parser.addOption(new Option("output", Type.INTEGER), "o");
-		assertEquals(parser.optionExists("output"), true);
-	}
+//	@Test
+//	public void testOne() {
+//		parser.addOption(new Option("output", Type.INTEGER), "O");
+//		parser.addOption(new Option("output", Type.INTEGER), "K");
+//		assertEquals(parser.optionOrShortcutExists("O"), true);
+//	}
+//	
+//	@Test
+//	public void testTwo() {
+//		parser.addOption(new Option("he%llo", Type.BOOLEAN), "k");
+//		assertEquals(parser.optionExists("he%llo"), true);
+//	}
 
+//	@Test
+//	public void testThree() {
+//		parser.addOption(new Option("Hello", Type.BOOLEAN), "k");
+//		parser.addOption(new Option("k", Type.INTEGER), "f");
+//		assertEquals(parser.optionOrShortcutExists("k"), true);
+//	}
 }
