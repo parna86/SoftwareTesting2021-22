38445f015ed7d13a8496fd00823a49f72fb413f8
Ap0806 <57600509+parna86@users.noreply.github.com>
2022-03-03 14:28:51 +0000

    Init comiit

--- src/test/java/st/Task1_1_FunctionalTest.java
+++ src/test/java/st/Task1_1_FunctionalTest.java
@@ -2,12 +2,22 @@ package st;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class Task1_1_FunctionalTest {
 
+	private Parser parser;
+	
+	@Before
+	public void setUp() {
+		parser = new Parser();
+	}
+	
 	@Test
-	public void test() {
+	public void testOne() {
+		parser.addOption(new Option("output", Type.INTEGER), "o");
+		assertEquals(parser.optionExists("output"), true);
 	}
 
 }
